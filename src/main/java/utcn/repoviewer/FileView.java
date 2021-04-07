37662430fc850d47800c6000fa817676e089cd9d
AndreiA <andrei.aldea01@yahoo.com>
2021-04-07 16:38:39 +0300

    Display image files in the file view

--- src/main/java/utcn/repoviewer/FileView.java
+++ src/main/java/utcn/repoviewer/FileView.java
@@ -8,13 +8,19 @@ package utcn.repoviewer;
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import org.fife.ui.rsyntaxtextarea.*;
@@ -28,24 +34,40 @@ import org.apache.commons.io.FilenameUtils;
 public class FileView extends JPanel{
     String pathToFileAbsolute;
     RSyntaxTextArea textAreaCode;
+    JLabel imageArea;
     String fileExtension;
     
-    public FileView(String studentName, String pathToFileAbsolute){
+    public FileView(String studentName, String pathToFileAbsolute, int numberOfStudents){
         this.setLayout(new BoxLayout (this, BoxLayout.Y_AXIS));
         JLabel labelStudentName = new JLabel(studentName);
         labelStudentName.setAlignmentX( Component.CENTER_ALIGNMENT );
         this.add(labelStudentName);
         this.fileExtension = FilenameUtils.getExtension(pathToFileAbsolute);
         this.pathToFileAbsolute = pathToFileAbsolute;
-        String fileContent = getFileContents(pathToFileAbsolute);
-        this.textAreaCode = new RSyntaxTextArea(fileContent);
-        RTextScrollPane sp = new RTextScrollPane(textAreaCode);  //for scroll
-        textAreaCode.setAlignmentX( Component.CENTER_ALIGNMENT );
-        if (fileContent.isEmpty()){
-            textAreaCode.setEnabled(false);
-        }
-        customizeFileView();
-        this.add(sp);
+        
+        if(isImage(fileExtension)){
+
+            this.imageArea = new JLabel();
+            Image image;
+            image = getImage(pathToFileAbsolute);
+            int height = (image.getHeight(this))/numberOfStudents;
+            int width = (image.getWidth(this))/numberOfStudents;
+            Icon icon = new ImageIcon(image.getScaledInstance(width,height, (java.awt.Image.SCALE_SMOOTH)));
+            imageArea.setIcon(icon);
+            imageArea.setAlignmentX( Component.CENTER_ALIGNMENT );
+            this.add(imageArea);
+
+        }else{
+                String fileContent = getFileContents(pathToFileAbsolute);
+                this.textAreaCode = new RSyntaxTextArea(fileContent);
+                RTextScrollPane sp = new RTextScrollPane(textAreaCode);  //for scroll
+                textAreaCode.setAlignmentX( Component.CENTER_ALIGNMENT );
+                if (fileContent.isEmpty()){
+                    textAreaCode.setEnabled(false);
+                }
+                customizeFileView();
+                this.add(sp);
+            }
 }
     public void customizeFileView(){
         if(fileExtension.equals("java")){  //check if the file is .java
@@ -77,6 +99,28 @@ public class FileView extends JPanel{
             Logger.getLogger(FileView.class.getName()).log(Level.SEVERE, null, ex);
         }
         return fileContent;
+    }
+    
+    public Image getImage(String absolutePath){
+        File f = new File(absolutePath);
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(f);
+        } catch (IOException ex) {
+            Logger.getLogger(FileView.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return image;
+    }
+    
+    private boolean isImage(String path){
+        boolean ret = false;
+        final String[] allowedFileTypes = new String[] {"jpg", "png"};
+            for(String types :allowedFileTypes){
+                if(path.equals(types)){
+                    ret = true;
+                }
+            }   
+        return ret;    
     }
     // student name
     // code area
